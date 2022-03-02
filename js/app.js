63ea2ac96f8a86f15df68869fc9768b9c0d90bbe
idilipsarker <dilipsarker719@gmail.com>
2022-03-02 22:10:16 +0600

    cards-details sections added

--- js/app.js
+++ js/app.js
@@ -14,10 +14,10 @@ const searchPhone = () => {
 
 //get  all phones from the data array
 const displaySearchResult = (phones) => {
-    console.log(phones)
+    // console.log(phones)
     const searchResult = document.getElementById("search-result");
     searchResult.textContent = "";
-    for (const phone of phones) {
+     phones.forEach(phone => {
         // console.log(phone);
         //create cards dynamically 
         const div = document.createElement("div");
@@ -28,19 +28,54 @@ const displaySearchResult = (phones) => {
                 <div class="card-body mx-auto">
                  <h5 class="card-title">${phone.phone_name}</h5>
                  <p class="card-text">${phone.brand}</p>
-                 <button  onclick="loadPhoneDetails(${phone.slug})" class="border-0 border-rounded card-button" >Explore more</button>
+                 <button onclick="loadPhoneDetails('${phone.slug}')" class="border-0 border-rounded card-button" ><a class="text-white text-decoration-none"href="#card-details-section">Explore more</a></button>
                 </div>
             </div>
             `;
             searchResult.appendChild(div)
-    }
+    })
 }
-const loadPhoneDetails = async slug => {
+const loadPhoneDetails =  slug => {
+    // console.log(slug)
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-    displayPhoneDetails(data);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetails(data.data))
+        
+    // const res = await fetch(url);
+    // const data = await res.json();
+    // displayPhoneDetails(data);
     
 
+}
+const displayPhoneDetails = phone => {
+    console.log(phone)
+    const mealDetails = document.getElementById("phone-details");
+    mealDetails.textContent = "";
+    const div = document.createElement("div");
+    div.classList.add('card');
+    div.classList.add('card-details-background');
+
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top w-50 mx-auto mt-3" alt="image-of-a-phone">
+    <div class="card-body" id="card-details-section">
+      <h5 class="card-title">${phone.name}</h5>
+      <p>Released Date: ${phone.releaseDate}</p>
+      <h5 class="card-title">Main Features:</h5>
+      <p class="card-text">Chip set: ${phone.mainFeatures.chipSet}</p>
+      <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
+      <p class="card-text">Display Size: ${phone.mainFeatures.displaySize}</p>
+      <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+      <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+      <h5 class="card-title">Sensors:</h5>
+      <p> ${phone.mainFeatures.sensors[0]},  ${phone.mainFeatures.sensors[1]}, ${phone.mainFeatures.sensors[2]}, ${phone.mainFeatures.sensors[3]}, ${phone.mainFeatures.sensors[4]}, ${phone.mainFeatures.sensors[5]}</p>
+      <h5 class="card-title">Others:</h5>
+      <p class="card-text">WLAN: ${phone.others.WLAN}</p>
+      <p class="card-text">Bluetooth: ${phone.others.Bluetooth}</p>
+      <p class="card-text">Bluetooth: ${phone.others.GPS}</p>
+      <p class="card-text">NFC: ${phone.others.NFC}</p>
+      <p class="card-text">Radio: ${phone.others.Radio}</p>
+    </div>
+    `;
+    mealDetails.appendChild(div)
 }
